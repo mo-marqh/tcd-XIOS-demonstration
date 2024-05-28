8743962568594428ef0a4cf8e061926b8f02c24c
mo-marqh <mark.hedley@metoffice.gov.uk>
2024-05-28 11:59:02 +0100

    domain setup failure

--- xios_examples/read_domain_decomp_resample/resample.F90
+++ xios_examples/read_domain_decomp_resample/resample.F90
@@ -69,32 +69,32 @@ contains
     print *, 'original_domain: rank,ni_glo,nj_glo,ni,nj,ibegin,jbegin ',rank,ni_glo,nj_glo,ni,nj,ibegin,jbegin
     allocate ( xvals(ni) )
     allocate ( yvals(nj) )
-    allocate ( xb(2, ni) )
-    allocate ( yb(2, nj) )
+    !allocate ( xb(2, ni) )
+    !allocate ( yb(2, nj) )
     call xios_get_domain_attr("odatax::", lonvalue_1d=xvals, &
         latvalue_1d=yvals)
     print *, 'odatax; rank', rank, 'xvals', xvals
     print *, 'odatax; rank', rank, 'yvals', yvals
-    call xios_get_domain_attr("odatax::", lonvalue_1d=xvals, &
-                              latvalue_1d=yvals, bounds_lon_1d=xb, &
-                              bounds_lat_1d=yb)
-    print *, 'odatax; rank', rank, 'xbounds', xb
-    print *, 'odatax; rank', rank, 'ybounds', yb
+    ! call xios_get_domain_attr("odatax::", lonvalue_1d=xvals, &
+    !                           latvalue_1d=yvals, bounds_lon_1d=xb, &
+    !                           bounds_lat_1d=yb)
+    ! print *, 'odatax; rank', rank, 'xbounds', xb
+    ! print *, 'odatax; rank', rank, 'ybounds', yb
 
     call xios_get_domain_attr("edatax::", &
                               ni_glo=ni_glo_r, nj_glo=nj_glo_r, &
                               ni=ni_r, nj=nj_r, &
                               ibegin=ibegin_r, jbegin=jbegin_r)
     print *, 'resampled_domain: rank,ni_glo,nj_glo,ni,nj,ibegin,jbegin ',rank,ni_glo_r,nj_glo_r,ni_r,nj_r,ibegin_r,jbegin_r
-    allocate ( xvals(ni_r) )
-    allocate ( yvals(nj_r) )
-    allocate ( xb(2, ni_r) )
-    allocate ( yb(2, nj_r) )
+    allocate ( xrvals(ni_r) )
+    allocate ( yrvals(nj_r) )
+    !allocate ( xb(2, ni_r) )
+    !allocate ( yb(2, nj_r) )
 
     call xios_get_domain_attr("edatax::", &
                               lonvalue_1d=xrvals, &
-                              latvalue_1d=yrvals, bounds_lon_1d=xrb, &
-                              bounds_lat_1d=yrb)
+                              latvalue_1d=yrvals)!, bounds_lon_1d=xrb, &
+                              !bounds_lat_1d=yrb)
 
     ! initialize the main context for interacting with the data.
     call xios_context_initialize('main', comm)
@@ -113,23 +113,28 @@ contains
     !                           ni=ni_r, nj=nj_r, &
     !                           ibegin=ibegin_r, jbegin=jbegin_r)
 
-    call xios_set_domain_attr("odata::", &
+    call xios_set_domain_attr("original_domain", &
                               ni_glo=ni_glo, nj_glo=nj_glo, &
                               ni=ni, nj=nj, &
                               ibegin=ibegin, jbegin=jbegin, &
                               lonvalue_1d=xvals, &
-                              latvalue_1d=yvals, bounds_lon_1d=xb, &
-                              bounds_lat_1d=yb)
-    call xios_set_domain_attr("edata::", &
+                              latvalue_1d=yvals)!, bounds_lon_1d=xb, &
+                              !bounds_lat_1d=yb)
+    call xios_set_domain_attr("resampled_domain", &
                               ni_glo=ni_glo_r, nj_glo=nj_glo_r, &
                               ni=ni_r, nj=nj_r, &
                               ibegin=ibegin_r, jbegin=jbegin_r, &
                               lonvalue_1d=xrvals, &
-                              latvalue_1d=yrvals, bounds_lon_1d=xrb, &
-                              bounds_lat_1d=yrb)
-
+                              latvalue_1d=yrvals)!, bounds_lon_1d=xrb, &
+                              !bounds_lat_1d=yrb)
+    print *, 'ici call xios_close_context_definition()'
     call xios_close_context_definition()
 
+    deallocate(xvals)
+    deallocate(yvals)
+    deallocate(xrvals)
+    deallocate(yrvals)
+    print *, 'ici  end subroutine initialise'
   end subroutine initialise
 
   subroutine finalise()
