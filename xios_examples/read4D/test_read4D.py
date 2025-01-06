8ae52edf0f6a7674e4db9f6e88cbb1f7a03e9c49
mo-marqh <87382490+mo-marqh@users.noreply.github.com>
2025-01-06 16:55:13 +0000

    try in situ syntax update for XIOS3 domain vname:: syntax

--- xios_examples/read4D/test_read4D.py
+++ xios_examples/read4D/test_read4D.py
@@ -26,3 +26,16 @@ class TestRead4D(xshared._TestCase):
         subprocess.run(['ncgen', '-k', 'nc4', '-o', inputfile,
                         infile], cwd=self.test_dir, check=True)
         self.run_mpi_xios()
+
+    @classmethod
+    def setUpClass(cls):
+        if os.environ.get('MVER', '').startswith('XIOS3/trunk'):
+            with open(os.path.join(cls.test_dir, 'read.F90'), 'r') as ioin:
+                iodef_in = ioin.read()
+            # patch in XIOS3 domain access source code syntax
+            in2 = 'xios_get_domain_attr("original_domain"'
+            in3 = ('xios_get_domain_attr("specific_humidity::"')
+            iodef_out = iodef_in.replace(in2, in3)
+            with open(os.path.join(cls.test_dir, 'read.F90'), 'w') as ioout:
+                ioout.write(iodef_out)
+        super().setUpClass()
